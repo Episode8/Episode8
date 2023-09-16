a9826ee3cc8586afe5c184d351c97e6aeb184214
Jianxi Liang <134390004+Episode8@users.noreply.github.com>
2023-09-16 18:42:37 +0800

    Add files via upload

---
+++ homework/src/papercheck/component/EnterNullException.java
@@ -0,0 +1,8 @@
+package src.papercheck.component;
+public class EnterNullException extends Exception {
+    public EnterNullException(){
+    }
+    public EnterNullException(String message){
+        super(message);
+    }
+}
